ff610dbc2c476e4e239d0b86ac2f51464390aef9
Nathan <hui.n1998@gmail.com>
2022-08-04 00:16:23 +0100

    no result component changes

--- components/NoResults/NoResults.tsx
+++ components/NoResults/NoResults.tsx
@@ -1,7 +1,19 @@
+import React from "react";
+import { MdOutlineVideocamOff } from "react-icons/md";
+
 interface IProps {
   text: string;
 }
 
-export default function NoResults({ text }: IProps) {
-  return <div>{text}</div>;
-}
+const NoResults = ({ text }: IProps) => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <p className="text-8xl">
+        <MdOutlineVideocamOff />
+      </p>
+      <p className="text-center text-2xl">{text}</p>
+    </div>
+  );
+};
+
+export default NoResults;
