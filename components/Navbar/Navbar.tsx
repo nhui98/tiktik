dae7c22b59d0066c6edfa802f307d9645d7d30b4
Nathan <hui.n1998@gmail.com>
2022-08-03 14:44:21 +0100

    navbar with google auth

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import useAuthStore from "../../store/authStore";
 
 import Logo from "../../utils/tiktik-logo.png";
+import { createOrGetUser } from "../../utils";
 
 export default function Navbar() {
+  const { userProfile, addUser, removeUser } = useAuthStore();
+
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+    <div className="flex w-full items-center justify-between border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
         <div className="w-[100px] md:w-[130px]">
           <Image
@@ -20,6 +24,51 @@ export default function Navbar() {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/upload">
+              <button className="text-md flex items-center justify-center gap-2 border-2 px-2 font-semibold md:px-4">
+                <IoMdAdd className="text-xl" />{" "}
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <Image
+                  width={40}
+                  height={40}
+                  className="cursor-pointer rounded-full"
+                  src={userProfile.image}
+                  alt="profile photo"
+                />
+              </Link>
+            )}
+            <button
+              type="button"
+              className="px-2"
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => {
+              createOrGetUser(response, addUser);
+            }}
+            onError={() => {
+              console.log("error");
+            }}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.stockvault.net"],
+    domains: ["www.stockvault.net", "lh3.googleusercontent.com"],
   },
 };
 
